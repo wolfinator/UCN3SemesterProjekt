58c5c94ac8c2f1532f94452b3f282f8ca8c7f87b
Mads <54942401+wolfinator@users.noreply.github.com>
2022-10-31 11:31:54 +0100

    added test for connecting to database

---
+++ Gruppe3BadmintonBooking/Tests/TestDataAccess.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Tests
+{
+    
+    public class TestDataAccess
+    {
+        /// <summary>
+        /// Test to connect to the database
+        /// </summary>
+        [Fact]
+        public void TestConnectToDatabase()
+        {
+            //Arrange
+            SqlConnectionStringBuilder conStr = new();
+            conStr.DataSource = "hildur.ucn.dk";
+            conStr.InitialCatalog = "DMA-CSD-S212_10407522";
+            conStr.Encrypt = false;
+            conStr.UserID = "DMA-CSD-S212_10407522";
+            conStr.Password = "Password1!";
+
+            int expected = 1;
+
+            SqlConnection con = new(conStr.ConnectionString);
+            SqlCommand cmd = new SqlCommand("select 1", con);
+            //Act
+            con.Open();
+            int actual = (int) cmd.ExecuteScalar();
+            con.Close();
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
