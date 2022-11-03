796de1ccd2592e1b3a08525dcab436c98178f657
DetSnitzel <10407522@ucn.dk>
2022-11-03 10:47:04 +0100

    Model layer classes

---
+++ Gruppe3BadmintonBooking/Model/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Person
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public String email { get; set; }
+        public String phoneNo { get; set; }
+        public String address { get; set; }
+    }
+}
---
+++ Gruppe3BadmintonBooking/Model/Reservation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Reservation
+    {
+        public DateTime dateTime { get; set; }
+        public int courtNo { get; set; }
+        public bool isEquipment { get; set; }
+        public Person customer { get; set; }
+        public Employee employee { get; set; }
+        public Court court { get; set; }
+    }
+}
