2d59ca0892ed3ec1b85e0c524ad1a3e55fc617f3
DetSnitzel <10407522@ucn.dk>
2022-11-03 10:47:04 +0100

    Model layer classes

---
+++ Gruppe3BadmintonBooking/Model/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Invoice
+    {
+        public decimal totalPrice { get; set; }
+        public int orderNo { get; set; }
+        public String name { get; set; }
+        public DateTime dateTime { get; set; }
+        public int courtNo { get; set; }
+        public String phoneNo { get; set; }
+        public Employee employee { get; set; }
+        public Court court { get; set; }
+    }
+}
