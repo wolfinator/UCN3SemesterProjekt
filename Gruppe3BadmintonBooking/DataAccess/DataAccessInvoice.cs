822ac9385fdc3c4717ff201d37faa9c5c2e5f32b
carolinebernat1 <10189126@ucn.dk>
2022-11-04 12:32:06 +0100

    Update DataAccessInvoice.cs
    Update delete

--- Gruppe3BadmintonBooking/DataAccess/DataAccessInvoice.cs
+++ Gruppe3BadmintonBooking/DataAccess/DataAccessInvoice.cs
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿using Microsoft.Data.SqlClient;
 using Model;
 using System;
@@ -468,3 +469,93 @@ namespace DataAccess
     }
 }
 >>>>>>> DataaccessInvoice mangler lidt crud,, og test for invoice påbegyndt
+=======
+﻿using Microsoft.Data.SqlClient;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DataAccessInvoice : IDataAccess<Invoice>
+    {
+        private SqlConnectionStringBuilder conStr;
+
+        public DataAccessInvoice()
+        {
+            conStr = Connection.conStr;
+        }
+
+        public void Create(Invoice entity)
+        {
+            SqlConnection con = new(conStr.ConnectionString);
+
+            string cmdTextInvoice = "insert into Invoice(total_price, person_id, reservation_id) " +
+                                     "values (@TotalPrice, @PersonId, @ReservationId) ";
+
+            SqlCommand cmdInvoice = new(cmdTextInvoice, con);
+
+            cmdInvoice.Parameters.AddWithValue("@totalPrice", entity.totalPrice);
+            cmdInvoice.Parameters.AddWithValue("@PersonId", entity.employee.id);
+            cmdInvoice.Parameters.AddWithValue("@ReservationId", entity.reservation);
+
+
+            con.Open();
+            try
+            {
+                cmdInvoice.ExecuteNonQuery();
+            }
+            catch (SqlException)
+            {
+                throw;
+                //TODO Skriv hvad der skal kastes
+            }
+            con.Close();
+
+
+        }
+
+        public void DeleteById(int id)
+        {
+
+            SqlConnection con = new(conStr.ConnectionString);
+
+            try
+            {
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "DELETE FROM Invoice WHERE Id = @id";
+                cmd.Parameters.AddWithValue("@id", id);
+
+                int rows = cmd.ExecuteNonQuery();
+
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            con.Close();
+
+        }
+
+        public IEnumerable<Invoice> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Invoice GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Invoice entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+>>>>>>> Update DataAccessInvoice.cs
