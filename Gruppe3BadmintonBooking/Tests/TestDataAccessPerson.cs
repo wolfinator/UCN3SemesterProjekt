9a90d4777fc6d15e4d7f9b0f7a82658b032de005
Mads Wolf <mwolf19@student.aau.dk>
2022-11-04 11:01:52 +0100

    added test to create person (needs cascade on db)

--- Gruppe3BadmintonBooking/Tests/TestDataAccessPerson.cs
+++ Gruppe3BadmintonBooking/Tests/TestDataAccessPerson.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using DataAccess;
+using Microsoft.Data.SqlClient;
 using Model;
 using System;
 using System.Collections.Generic;
@@ -14,16 +15,51 @@ namespace Tests
         public void TestGetPersonType()
         {
             //Arrange
+            DataAccessPerson dataAccessPerson = new();
+
             Person employee = new Employee();
             Person guest = new Guest();
             Person member = new Member();
             Person person = new Person();
 
             int expectedEmployee = 0;
+            int expectedGuest = 1;
+            int expectedMember = 2;
+            int expectedPerson = -1;
             //Act
+            int actualEmployee = dataAccessPerson.GetPersonType(employee);
+            int actualGuest = dataAccessPerson.GetPersonType(guest);
+            int actualMember = dataAccessPerson.GetPersonType(member);
+            int actualPerson = dataAccessPerson.GetPersonType(person); ;
 
             //Assert
-            
+            Assert.Equal(expectedEmployee, actualEmployee);
+            Assert.Equal(expectedGuest, actualGuest);
+            Assert.Equal(expectedMember, actualMember);
+            Assert.Equal(expectedPerson, actualPerson);
+        }
+        [Fact]
+        public void TestCreateMember()
+        {
+            //Arrange
+            DataAccessPerson dataAccessPerson = new();
+            Person member = new Member() {firstName = "test", lastName = "Wazowski", email = "wazowski@mail.dk", phoneNo = "12345678", street = "Mike Wazowskis Vej", houseNo = "45", zipcode = "9220"};
+
+            SqlConnection con = new(Connection.conStr.ConnectionString);
+            SqlCommand cmd = new("select * from Person where f_name = 'test' and email = 'wazowski@mail.dk'", con);
+            SqlCommand cleanup = new("delete from Person where f_name = 'test' and email = 'wazowski@mail.dk'", con);
+            //Act
+            dataAccessPerson.Create(member);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            //Assert
+            Assert.True(reader.Read());
+
+            //Cleanup
+            reader.Close();
+            cleanup.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
