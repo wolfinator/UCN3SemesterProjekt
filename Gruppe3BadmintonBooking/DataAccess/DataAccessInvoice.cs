13aa8f26a0fed0487fd2627e2d48407df485f40c
LessIsMore42069 <93514785+LessIsMore42069@users.noreply.github.com>
2022-11-04 12:04:27 +0100

    DataaccessInvoice mangler lidt crud,, og test for invoice påbegyndt

--- Gruppe3BadmintonBooking/DataAccess/DataAccessInvoice.cs
+++ Gruppe3BadmintonBooking/DataAccess/DataAccessInvoice.cs
@@ -48,7 +48,8 @@ namespace DataAccess
 
         public void DeleteById(int id)
         {
-           
+           bool isSuccesfull = false;
+
             SqlConnection con = new(conStr.ConnectionString);
 
             try
@@ -56,13 +57,33 @@ namespace DataAccess
                 con.Open();
                 SqlCommand cmd = con.CreateCommand();
                 cmd.CommandText = "DELETE FROM Invoice WHERE Id = @id";
+                string sql = "DELETE FROM invoice WHERE id = @id";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+                //Passing the value using cmd
                 cmd.Parameters.AddWithValue("@id", id);
+
+                //Open sql Connection
+                con.Open();
 
                int rows = cmd.ExecuteNonQuery();
 
+                //If the query is executed successfully then the value of rows will be greater than zero
+                //else it will be less than 0
+                if(rows > 0)
+                {
+                    //Query Executed Successfully
+                    isSuccesfull = true;
+                } 
+                else
+                {
+                    //Faied to Execute Query
+                    isSuccesfull = false;
+                }
             }
-            catch (SqlException) 
+            catch (SqlException)
             {
+
                 throw;
             }
 
