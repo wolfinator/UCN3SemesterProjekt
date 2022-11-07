adce90c20512a56f0fe0e4bae960855bb254d884
DetSnitzel <10407522@ucn.dk>
2022-11-07 13:45:17 +0100

    Reservation Test NOT DONE

---
+++ Gruppe3BadmintonBooking/Tests/TestDataAccessReservation.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Model;
+
+namespace Tests
+{
+    public class TestDataAccessReservation
+    {
+        private SqlConnectionStringBuilder conStr;
+
+        public void TestCreateReservation()
+        {
+            //Arrange
+            TestDataAccessReservation dataAccessReservation = new();
+            Reservation reservation = new()
+            {
+                dateTime = DateTime.Now,
+                courtNo = 1,
+                isEquipment = true,
+
+                //customer = new Person(),
+                //employee = new Employee(),
+
+            };
+            SqlConnection con = new(conStr.ConnectionString);
+            SqlCommand cmdTest = new("select * from Reservation where id");
+
+
+            //string cmdTextReservationTest = "insert into Reservation(date_time, is_equipment, court_id) " +
+            //                                "values (@DateTime, @IsEquipment, @CourtId)";
+            //SqlCommand cmdReservationTest = new(cmdTextReservationTest, con);
+            //cmdReservationTest.Parameters.AddWithValue("@DateTime", reservation.dateTime);
+            //cmdReservationTest.Parameters.AddWithValue("@IsEquipment", reservation.isEquipment);
+            //cmdReservationTest.Parameters.AddWithValue("@CourtId", reservation.courtNo);
+
+
+            //Act
+            
+
+            con.Open();
+
+            try
+            {
+                cmdTest.ExecuteNonQuery();
+            }
+            catch (SqlException)
+            {
+                throw new NotImplementedException(); //TODO SKRIV throw ting
+            }
+            //Assert
+
+        }
+
+        public void TestDeleteById()
+        {
+
+        }
+
+        public void TestGetAll()
+        {
+
+        }
+
+        public void TestGetById()
+        {
+
+        }
+
+        public void TestUpdate()
+        {
+
+        }
+    }
+}
