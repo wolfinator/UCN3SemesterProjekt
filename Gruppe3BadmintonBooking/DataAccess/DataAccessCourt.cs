8d74ddf626921c27c60648a6892bbf0732149d76
DetSnitzel <10407522@ucn.dk>
2022-11-17 12:44:59 +0100

    dataccess court

---
+++ Gruppe3BadmintonBooking/DataAccess/DataAccessCourt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace DataAccess
+{
+    public class DataAccessCourt : IDataAccess<Court>
+    {
+        public bool Create(Court entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Court> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Court GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Court entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
