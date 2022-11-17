dd507f2fedbe95ad618e05de0a7f9fa4cd1014a9
Mads <54942401+wolfinator@users.noreply.github.com>
2022-11-17 12:57:20 +0100

    almost done first step

--- Gruppe3BadmintonBooking/WebApi/Program.cs
+++ Gruppe3BadmintonBooking/WebApi/Program.cs
@@ -1,11 +1,17 @@
+using DataAccess;
+using Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IDataAccess<Person>, DataAccessPerson>();
+builder.Services.AddSingleton<IDataAccess<Reservation>, DataAccessReservation>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
