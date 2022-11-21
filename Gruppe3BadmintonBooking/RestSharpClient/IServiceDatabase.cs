af19a1ce3a60a981acb3bd749d18406f167b5e56
Annafalgren <93514782+Annafalgren@users.noreply.github.com>
2022-11-21 12:25:18 +0100

    courtservice

---
+++ Gruppe3BadmintonBooking/RestSharpClient/IServiceDatabase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestSharpClient
+{
+    public interface IServiceDatabase<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        bool Update(T entity);
+        bool DeleteById(int id);
+        bool Create(T entity);
+    }
+}
