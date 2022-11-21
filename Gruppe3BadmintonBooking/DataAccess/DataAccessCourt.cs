0831a3276308191f02ed391ff859fa1c3b58a046
Annafalgren <93514782+Annafalgren@users.noreply.github.com>
2022-11-21 11:24:15 +0100

    WebApi changes

--- Gruppe3BadmintonBooking/DataAccess/DataAccessCourt.cs
+++ Gruppe3BadmintonBooking/DataAccess/DataAccessCourt.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 using Model;
 
 namespace DataAccess
 {
     public class DataAccessCourt : IDataAccess<Court>
     {
-        public bool Create(Court entity)
+        private SqlConnectionStringBuilder conStr;
+        public DataAccessCourt()
         {
+            conStr = Connection.conStr;
+        }
+
+        public bool Create(Court court)
+        {
+            SqlConnection con = new(conStr.ConnectionString);
+            con.Open();
+
             throw new NotImplementedException();
         }
 
@@ -21,13 +32,48 @@ namespace DataAccess
 
         public IEnumerable<Court> GetAll()
         {
-            throw new NotImplementedException();
+            SqlConnection con = new(conStr.ConnectionString);
+            con.Open();
+
+            List<Court> list = new List<Court>();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT * from Court";
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Court court = new Court()
+                {
+                    id = reader.GetInt32(0),
+                    hallNo = reader.GetInt32(1)
+                };
+                list.Add(court);
+            }
+            con.Close();
+            return list;
         }
 
         public Court GetById(int id)
         {
-            throw new NotImplementedException();
+            Court court = null;
+            SqlConnection con = new(conStr.ConnectionString);       
+            con.Open();
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = " SELECT * FROM Court where court.id = @id";
+            cmd.Parameters.AddWithValue("id", id);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                court = new Court();
+                court.id = reader.GetInt32(0);
+                court.hallNo = reader.GetInt32(1); 
+            }
+            return court;
         }
+        
+
 
         public bool Update(Court entity)
         {
--- Gruppe3BadmintonBooking/WebApi/Controllers/CourtsController.cs
+++ Gruppe3BadmintonBooking/WebApi/Controllers/CourtsController.cs
@@ -11,9 +11,12 @@ namespace WebApi.Controllers
     public class CourtsController : ControllerBase
     {
         private readonly IDataAccess<Court> _courtDb;
+        private readonly ILogger<CourtsController> _logger;
 
-        public CourtsController(IDataAccess<Court> courtDb)
+
+        public CourtsController(ILogger<CourtsController> logger, IDataAccess<Court> courtDb)
         {
+            _logger = logger;
             _courtDb = courtDb;
         }
 
@@ -28,27 +31,54 @@ namespace WebApi.Controllers
 
         // GET api/<CourtsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Court Get(int id)
         {
-            return "value";
+            return _courtDb.GetById(id);
         }
 
         // POST api/<CourtsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Court court)
         {
+            try
+            {
+                _courtDb.Create(court);
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, $"Failed to post court in database: \n{ex.Message}");
+
+            }
+          
         }
 
         // PUT api/<CourtsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Court court)
         {
+            try
+            {
+                _courtDb.Update(court);
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, $"Failed to put court in database: \n{ex.Message}");
+            }
         }
 
         // DELETE api/<CourtsController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            try
+            {
+                _courtDb.DeleteById(id);
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, $"Failed to delete court in database: \n{ex.Message}");
+
+            }
         }
     }
 }
