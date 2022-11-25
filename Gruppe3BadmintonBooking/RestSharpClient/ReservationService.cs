fdeb53a90e49bfa656cf61f95cd3f24ed7b3073f
carolinebernat1 <10189126@ucn.dk>
2022-11-25 09:23:46 +0100

    ReservationService

---
+++ Gruppe3BadmintonBooking/RestSharpClient/ReservationService.cs
@@ -0,0 +1,43 @@
+﻿using Model;
+using RestSharp;
+
+namespace RestSharpClient
+{
+    public class ReservationService : IServiceDatabase<Reservation>
+    {
+
+        private RestSharp.RestClient _restClient;
+
+        public ReservationService()
+        {
+            _restClient = new RestSharp.RestClient("https://localhost:44325/api/reservations");
+        }
+
+        public bool Create(Reservation entity)
+        {
+            _restClient.Post<Reservation>(new RestRequest());
+          
+        }
+
+        public bool DeleteById(int id)
+        {
+            _restClient.Delete<Reservation>(new RestRequest());
+        }
+
+        public IEnumerable<Reservation> GetAll()
+        {
+            return _restClient.Get<IEnumerable<Reservation>>(new RestRequest());
+
+        }
+
+        public Reservation GetById(int id)
+        {
+            return _restClient.Get<Reservation>(new RestRequest());
+        }
+
+        public bool Update(Reservation entity)
+        {
+            _restClient.Put<Reservation>(new RestRequest());
+        }
+    }
+}
